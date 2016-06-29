e2800606520a005241f325eb85588c58c580fa0a
Hari <me@hsr.co.in>
2016-06-29 21:18:57 +0530

    restructured app to include es6

--- browserify.js
+++ browserify.js
@@ -1,17 +1,18 @@
 var fs = require('fs'),
-    browserify = require('browserify');
+    browserify = require('browserify'),
+    watchify = require('watchify');
 
 var builder = browserify({
-    entries: ['./src/mongoose-schema-generator.es6'],
-    debug: true
-}).transform("babelify", {presets: ["es2015"], sourceMaps: false});
+        entries: ['./src/mongoose-schema-generator.es6'],
+        debug: true,
+        cache: {},
+        packageCache: {},
+        plugin: [watchify]
+    }).transform("babelify", {presets: ["es2015"], sourceMaps: false}),
+    bundle = function () {
+        builder.bundle().pipe(fs.createWriteStream('./dist/mongoose-schema-generator.js'));
+        console.log("BUILD COMPLETE");
+    };
 
-builder.bundle(function (err, buff) {
-    if(err){
-        console.log(err);
-    } else {
-        fs.writeFile('./dist/mongoose-schema-generator.js', buff.toString(), function () {
-            console.log("BUILD COMPLETE");
-        });
-    }
-});
+builder.on('update', bundle);
+bundle();
--- src/mongoose-schema-generator.es6
+++ src/mongoose-schema-generator.es6
@@ -1,3 +1,16 @@
 import {appName} from './config.es6';
 
-let test = "hello " + appName;
+class mongooseSchemaGenerator {
+    constructor(name){
+        this.appName = name + ' webapp';
+        this.version = '0.0.1';
+    }
+    start(){
+        console.info(["Start:", this.appName, this.version].join(" "));
+    }
+}
+
+let MSG = new mongooseSchemaGenerator(appName);
+
+window['$MSG'] = MSG;
+MSG.start();
